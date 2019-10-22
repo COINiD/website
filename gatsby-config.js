3487e6a10bc82963c64e85737be77ca407764f3c
Emil Karlsson <emil.karl@gmail.com>
2019-10-22 21:34:03 +0200

    Add font preload

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-preload-fonts`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
