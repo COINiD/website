0b33bc8b63b3619d3e770e13a157fdf626185331
Emil Karlsson <emil.karl@gmail.com>
2019-10-22 21:34:40 +0200

    Remove SSR font stuff

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -11,39 +11,3 @@
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
-
-const React = require('react')
-const fs = require('fs')
-
-exports.onRenderBody = ({ setHeadComponents }) => {
-  const files = getFilesFromPath('./public/static', '.woff2')
-  const preload = ['Inter-Regular', 'Inter-Bold']
-
-  setHeadComponents([
-    files.map(file => {
-      return preload.map((font, key) => {
-        const fileBeginning = file
-          .split('-')
-          .slice(0, -1)
-          .join('-')
-        if (fileBeginning === font) {
-          return (
-            <link
-              key={key}
-              rel="preload"
-              as="font"
-              type="font/woff2"
-              crossOrigin="anonymous"
-              href={`/static/${file}`}
-            />
-          )
-        }
-      })
-    }),
-  ])
-}
-
-function getFilesFromPath(path, extension) {
-  let dir = fs.readdirSync(path)
-  return dir.filter(elm => elm.match(new RegExp(`.*.(${extension})`, 'ig')))
-}
